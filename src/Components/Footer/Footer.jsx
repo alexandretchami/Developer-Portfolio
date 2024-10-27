4f775ab613ae7a659e4cc157561f76d49728a3c0
Alexandre TCHAMI <58574953+alexandretchami@users.noreply.github.com>
2024-10-27 07:51:34 +0100

    Initial commit

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,36 +1,44 @@
-import './Footer.css'
-import footer_logo from '../../assets/footer_logo.png'
-import user_icon from '../../assets/user_icon.svg' 
-
+import "./Footer.css";
+import footer_logo from "../../assets/footer_logo.png";
+import user_icon from "../../assets/user_icon.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Footer = () => {
-
   return (
-    <div className='footer'>
-        <div className="footer-top">
-            <div className="footer-top-left">
-                <img src={footer_logo} alt="" />
-                <p>Je suis un Développeur Web originaire du CAMEROUN avec 05 ans d&apos;expérience.</p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    <img src={user_icon} alt="" />
-                    <input type="email" placeholder='Entrez votre email'/>
-                </div>
-                <div className="footer-subscribe">S&apos;abonner</div>
-            </div>
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>
+            Je suis un Développeur Web originaire du CAMEROUN avec 05 ans
+            d&apos;expérience.
+          </p>
         </div>
-        <hr />
-        <div className="footer-bottom">
-            <div className="footer-bottom-left">© 2022 TchamiDev. Tous droits réservés.</div>
-            <div className="footer-bottom-right">
-                <p>Conditions d&apos;utilisation</p>
-                <p>politique de confidentialité</p>
-                <p>Connectez-vous avec moi</p>
-            </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" placeholder="Entrez votre email" />
+          </div>
+          <div className="footer-subscribe">S&apos;abonner</div>
         </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <div className="footer-bottom-left anchor-link">
+          © 2022 Tous droits réservés | TchamiDev.
+        </div>
+        <div className="footer-bottom-right">
+          <p className="anchor-link">Conditions d&apos;utilisation</p>
+          <p className="anchor-link">politique de confidentialité</p>
+          <p>
+            <AnchorLink className="anchor-link" href="#contact">
+              Connectez-vous avec moi
+            </AnchorLink>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
